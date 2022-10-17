ab1361e018e24e59a87488dea22ae4629384899c
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-10-16 23:19:44 -0300

    feat: set cors *

--- src/app.js
+++ src/app.js
@@ -10,6 +10,14 @@ db.once("open", () => {
  * Start to use express and routes
  */
 const app = express();
+
+app.all('*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
 app.use( express.json() )
 routes(app);
 
