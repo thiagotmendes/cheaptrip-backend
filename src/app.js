b6c6f4586afae351056751d9b02da969bda187c9
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-11-06 21:16:03 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js"
+import cors from 'cors'
 
 db.on("error", console.log.bind(console, 'Erro de conexão: '))
 db.once("open", () => {
@@ -11,13 +12,7 @@ db.once("open", () => {
  */
 const app = express();
 
-app.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
-
+app.use(cors())
 app.use( express.json() )
 routes(app);
 
